2e45808cd849503794d570eccfcb3565a60e5334
userwaterzz <userwaterzz@163.com>
2022-04-09 20:37:59 +0800

    初始版本

---
+++ src/main/java/com/mathematics/calculate/ComputationalMathematicsApplication.java
@@ -0,0 +1,13 @@
+package com.mathematics.calculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputationalMathematicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputationalMathematicsApplication.class, args);
+	}
+
+}
